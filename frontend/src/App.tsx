c824f69fdb4a74133e2d4edfa924546319148114
Jonas Jönsson <jonas.jonsson@live.com>
2025-10-02 20:49:04 +0200

    Frontend baseline setup: Vite, Router, Tailwind, Motion, Zustand

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <h2 className="font-semibold text-2xl">APP.tsx</h2>
+    </div>
+  );
+}
+
+export default App;
