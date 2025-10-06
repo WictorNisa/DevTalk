1e290aa5abd21756c9db04ba61e858baa52d383d
Jonas Jönsson <jonas.jonsson@live.com>
2025-10-06 14:22:42 +0200

    feat: Built logo panel for the dash/chat page

--- frontend/src/components/dashboard/left/LeftTopWidget.tsx
+++
@@ -1,7 +0,0 @@
-const LeftTopWidget = () => {
-  return (
-    <div className="flex h-1/12 w-full flex-col bg-[var(--surface-md-dark)]"></div>
-  );
-};
-
-export default LeftTopWidget;
---
+++ frontend/src/components/dashboard/left/LogoPanel.tsx
@@ -0,0 +1,14 @@
+import DevTalkLogo from "@assets/img/devtalk-logo.svg?react";
+
+const LogoPanel = () => {
+  return (
+    <div className="flex h-1/12 w-full flex-col rounded-lg border bg-[var(--surface-md-dark)] p-1">
+      <div className="flex h-full items-center justify-center bg-[var(--surface-dark)] text-white opacity-90">
+        <DevTalkLogo className="h-full" />
+        <h1 className="px-1 pt-2 font-['Fira_Mono'] text-2xl">DEVTALK</h1>
+      </div>
+    </div>
+  );
+};
+
+export default LogoPanel;
