5618be19fd7a4ea7ebb95592dbd6db25c7465b09
Luke <lukesalem2002@gmail.com>
2025-10-07 21:41:46 +0200

    Renamed file to resolve conflicts

--- backend/src/main/java/com/devtalk/BackendApplication.java
+++ backend/src/main/java/com/devtalk/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.devtalk.backend;
+package com.devtalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/com/devtalk/config/WebSocketEventListener.java
+++ backend/src/main/java/com/devtalk/config/WebSocketEventListener.java
@@ -1,4 +1,4 @@
-package com.devtalk.backend.config;
+package com.devtalk.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
