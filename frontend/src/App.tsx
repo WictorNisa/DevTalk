a18585cb63c66e0af32257eed3250c085039e492
Jonas <jonas.jonsson@live.com>
2025-10-17 14:45:08 +0200

    branch init.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,9 +27,9 @@ const App = () => {
         <Route
           path={ROUTES.DASHBOARD}
           element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
+            //<ProtectedRoute>
+            <Dashboard />
+            //</ProtectedRoute>
           }
         />
 
