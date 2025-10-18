10302bf84369dc56bd2269555581a9f6a804a9cd
Jonas <jonas.jonsson@live.com>
2025-10-18 14:28:26 +0200

    feat: shadcn base outline for dashboard/chat

--- frontend/src/components/dashboard/left/LogoPanel.tsx
+++ frontend/src/components/dashboard/left/LogoPanel.tsx
@@ -1,15 +1,13 @@
 import DevTalkLogo from "@assets/img/devtalk-logo.svg?react";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 const LogoPanel = () => {
   return (
-    <Card>
-      <div className="flex h-1/12 w-full flex-col rounded-lg border bg-[var(--surface-md-dark)] p-1">
-        <div className="flex h-full items-center justify-center bg-[var(--surface-dark)] text-white opacity-90">
-          <DevTalkLogo className="h-full" />
-          <h1 className="px-1 pt-2 font-['Fira_Mono'] text-2xl">DEVTALK</h1>
-        </div>
-      </div>
+    <Card className="h-1/12 p-0">
+      <CardContent className="flex h-full items-center justify-center">
+        <DevTalkLogo className="h-full" />
+        <h1 className="px-1 text-3xl">DEVTALK</h1>
+      </CardContent>
     </Card>
   );
 };
