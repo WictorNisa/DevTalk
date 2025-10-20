d37ad93c40a680cdb7d23236e5f85c5e3099c9e6
Jonas <jonas.jonsson@live.com>
2025-10-20 19:42:02 +0200

    Custom sidebar setup.

--- frontend/src/components/dashboard/right/RightDashPanel.tsx
+++ frontend/src/components/dashboard/right/RightDashPanel.tsx
@@ -1,7 +1,7 @@
-import { Card } from "@/components/ui/card";
+import { Card } from "@components/ui/card";
 
 const RightDashPanel = () => {
-  return <Card className="flex h-full w-1/5"></Card>;
+  return <Card className="flex h-full w-full"></Card>;
 };
 
 export default RightDashPanel;
