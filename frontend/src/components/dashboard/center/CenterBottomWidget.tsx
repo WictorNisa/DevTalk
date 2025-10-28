75a64a93a66f1b8019602ad3237f6d28e6399afa
Nicholas Sjöstrand <kaigan.codes@gmail.com>
2025-10-28 12:20:15 +0100

    Shadcn v2 (#45)
    * branch init
    
    * Tailwind class name changes
    
    * font changes
    
    * package-lock.json fix
    
    * Buttons WIP
    
    * Button color changes
    
    * Added framer motion to cards and hero section
    
    * hero video changes
    
    * Added i18n language translations
    
    * Fixed the icons in features.tsx
    
    * Minor changes
    
    * Motion changes
    
    * dashboard fixes
    
    * Das ist gut
    
    * Added latest Dashboard changes
    
    ---------
    
    Co-authored-by: adhSwede <jonas.jonsson@live.com>
    Co-authored-by: Nicholas <kaigan.codes@aol.com>

--- frontend/src/components/dashboard/center/CenterBottomWidget.tsx
+++ frontend/src/components/dashboard/center/CenterBottomWidget.tsx
@@ -2,59 +2,76 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupButton,
+  // InputGroupText,
   InputGroupTextarea,
 } from "@/components/ui/input-group";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { ArrowUpIcon, PlusIcon } from "lucide-react";
+} from "@/components/ui/dropdown-menu";
+import { SmilePlus, PlusIcon, SendHorizontal, FileUp } from "lucide-react";
 
 const CenterBottomWidget = () => {
   return (
-    <InputGroup>
-      <InputGroupTextarea
-        className="content- min-h-21"
-        placeholder="Type your message here..."
-      />
-      <InputGroupAddon align="block-end">
-        {/* Add Icon */}
-        <InputGroupButton
-          variant="outline"
-          className="rounded-full"
-          size="icon-xs"
-        >
-          <PlusIcon />
-        </InputGroupButton>
-        {/* Drop Down */}
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <InputGroupButton variant="ghost">Auto</InputGroupButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            side="top"
-            align="start"
-            className="[--radius:0.95rem]"
+    <div className="grid w-full">
+      <InputGroup className="rounded-xl">
+        <InputGroupTextarea
+          className="min-h-auto"
+          placeholder="Type your message here..."
+        />
+        <InputGroupAddon align="block-end" className="ml-auto border-t">
+          {/* Drop Down */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <InputGroupButton
+                variant="outline"
+                className="cursor-pointer rounded-full"
+                size="icon-xs"
+              >
+                <PlusIcon />
+              </InputGroupButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              side="top"
+              align="start"
+              className="hidden w-48 md:block"
+            >
+              <DropdownMenuItem className="cursor-pointer">
+                <FileUp />
+                Upload File
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer">
+                Test
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer">
+                Test
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* Send Button */}
+          <InputGroupButton
+            variant="ghost"
+            className="ml-auto cursor-pointer rounded-full"
+            size="icon-xs"
           >
-            <DropdownMenuItem>Auto</DropdownMenuItem>
-            <DropdownMenuItem>Agent</DropdownMenuItem>
-            <DropdownMenuItem>Manual</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        {/* Send Button */}
-        <InputGroupButton
-          variant="default"
-          className="rounded-full"
-          size="icon-xs"
-          disabled
-        >
-          <ArrowUpIcon />
-          <span className="sr-only">Send</span>
-        </InputGroupButton>
-      </InputGroupAddon>
-    </InputGroup>
+            <SmilePlus />
+          </InputGroupButton>
+          <InputGroupButton
+            variant="ghost"
+            className="cursor-pointer rounded-full"
+            size="icon-xs"
+          >
+            <SendHorizontal />
+          </InputGroupButton>
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
   );
 };
 
--- frontend/src/components/dashboard/right/RightDashPanel.tsx
+++ frontend/src/components/dashboard/right/RightDashPanel.tsx
@@ -1,7 +1,14 @@
-import { Card } from "@components/ui/card";
+import { Card, CardContent } from "@components/ui/card";
+import { UserList } from "./UserList";
 
 const RightDashPanel = () => {
-  return <Card className="flex h-full w-full"></Card>;
+  return (
+    <Card className="flex h-full w-full p-1">
+      <CardContent className="p-1">
+        <UserList />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default RightDashPanel;
