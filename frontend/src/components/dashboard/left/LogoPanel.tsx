ee483293ad0c8748be17fdddabc47456c61606d2
Nicholas <kaigan.codes@aol.com>
2025-11-02 15:29:12 +0100

    Change of logo height

--- frontend/src/components/dashboard/left/LogoPanel.tsx
+++ frontend/src/components/dashboard/left/LogoPanel.tsx
@@ -7,14 +7,16 @@ import { NavLink } from "react-router";
 const LogoPanel = () => {
   const { isLeftCollapsed } = useSidebarStates();
   return (
-    <Card className="h-1/12 rounded-lg p-0">
+    <Card className="h-12 rounded-lg p-0">
       <CardContent className="flex h-full items-center justify-center p-0">
         <NavLink
           to={ROUTES.HOME}
           className="flex items-center justify-center p-2"
         >
-          <DevTalkLogo className="h-8 w-8" />
-          {!isLeftCollapsed && <h1 className="px-1 text-xl">DevTalk</h1>}
+          <DevTalkLogo className="h-9 w-9" />
+          {!isLeftCollapsed && (
+            <h1 className="px-2 text-lg font-medium">DevTalk</h1>
+          )}
         </NavLink>
       </CardContent>
     </Card>
