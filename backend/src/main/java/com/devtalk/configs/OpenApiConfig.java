e457023f313c575828ab198511685c44de4ad22d
Oskar <oskarlindahl03@gmail.com>
2025-11-18 16:24:35 +0100

    WIP

---
+++ backend/src/main/java/com/devtalk/configs/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.devtalk.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${spring.application.name:DevTalk API}")
+    private String applicationName;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(applicationName)
+                        .version("1.0.0")
+                        .description("API documentation for DevTalk application")
+                        .contact(new Contact()
+                                .name("DevTalk Team")
+                                .email("support@devtalk.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Local development server")
+                ));
+    }
+}
+
+
